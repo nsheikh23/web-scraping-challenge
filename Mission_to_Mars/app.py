fc826900f6d8bb09b717f0e3bf0d2b2cae5184b5
nsheikh23 <nabi.sheikh23@gmail.com>
2020-09-10 13:07:04 -0700

    Reverted the file location

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -2,11 +2,8 @@ from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
-# Setting path to template index.html file
-template_dir = '..'
-
 # Create an instance of Flask
-app = Flask(__name__, template_folder=template_dir)
+app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
 mongo = PyMongo(app, uri='mongodb://localhost:27017/mars_app')
